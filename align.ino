86841d739c788749a72706a84a208213d8eeb399
walczakp <walczakp>
2019-05-05 17:31:56 +0200

    finally, quite nice alignment of my tuner. doesn't hurt to have a backup on the repo.

--- align.ino
+++ align.ino
@@ -2,43 +2,53 @@ void align(uint32_t freq)
 {
     // alignment of the antenna circuit
     // these values are individual for each tuner!
-
-    if(freq>=107300)
+    
+    if(freq>=125000)
+        DAA = 46;
+    else if(freq>=125000)
+        DAA = 47;
+    else if(freq>=116000)
+        DAA = 48;
+    else if(freq>=110500)
+        DAA = 49;
+    else if(freq>=106000)
         DAA = 50;
-    else if(freq>=104500)
-        DAA = 51;
-    else if(freq>=103300)
+    else if(freq>=102000)
         DAA = 51;
     else if(freq>=100400)
         DAA = 52;
-    else if(freq>=97500)
+    else if(freq>=98500)
         DAA = 52;
-    else if(freq>=95600)
+    else if(freq>=93000)
         DAA = 53;
-    else if(freq>=92900)
+    else if(freq>=92000)
         DAA = 54;
-    else if(freq>=90500)
+    else if(freq>=90000)
         DAA = 55;
-    else if(freq>=87800)
-        DAA = 56;
     else if(freq>=87000)
-        DAA = 58;
+        DAA = 56;
     else if(freq>=85000)
+        DAA = 57;
+    else if(freq>=82000)
+        DAA = 58;
+    else if(freq>=79000)
+        DAA = 59;
+    else if(freq>=71000)
         DAA = 60;
-    else if(freq>=84000)
-        DAA = 61;
-    else if(freq>=83000)
-        DAA = 62;
-    else if(freq>=78000)
-        DAA = 63;
-    else if(freq>=76000)
-        DAA = 64;
-    else if(freq>=74000)
-        DAA = 65;
-    else if(freq>=73000)
-        DAA = 66;
     else if(freq>=70000)
-        DAA = 67;
+        DAA = 59;
+    else if (freq>=68500)
+        DAA = 59;
+    else if (freq>=69000)
+        DAA = 58;
+    else if (freq>=68500)
+        DAA = 57;
+    else if (freq>=68000)
+        DAA = 56;
+    else if (freq>=67500)
+        DAA = 55;
+    else if (freq>=67200)
+        DAA = 54;
     else
-        DAA = 68;
+        DAA = 53;
 }
